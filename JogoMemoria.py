d742b941e18c0a8c61b17cdfbae05c45b5dfe1b6
Alanis <160151740+AlanisMaria@users.noreply.github.com>
2025-08-08 12:01:46 -0300

    Update JogoMemoria.py

--- JogoMemoria.py
+++ JogoMemoria.py
@@ -33,29 +33,29 @@ class JogoDaMemoria:
         if len(self.cartas_viradas) < 2:
             self.exibir_tabuleiro()
             try:
-                linha = int(input("Escolha a linha (1-4): ")) - 1
-                coluna = int(input("Escolha a coluna (1-4): ")) - 1
+                linha = int(input("Escolha a linha (1-4): ")) - 1 #mostra ao usuário para escolher uma linha
+                coluna = int(input("Escolha a coluna (1-4): ")) - 1 #mostra ao usuário para escolher uma coluna
                 carta = self.tabuleiro[linha][coluna]
-                if carta.virada:
+                if carta.virada: #verifica se a carta já foi virada
                     print("Carta já virada. Tente outra.")
                     continue
                 carta.virar()
                 self.cartas_viradas.append(carta)
-            except (IndexError, ValueError):
+            except (IndexError, ValueError): #verifica se há erro de valor válido, ou tentativa de acessar um ítem que não existe  
                 print("Entrada inválida. Tente novamente.")
                 continue
 
-        if len(self.cartas_viradas) == 2:
+        if len(self.cartas_viradas) == 2: 
             self.exibir_tabuleiro()  # Exibe as duas cartas viradas
             c1, c2 = self.cartas_viradas
-            if c1.caractere == c2.caractere:
-                print("Você encontrou um par!")
+            if c1.caractere == c2.caractere: # verifica se as duas cartas viradas são iguais
+                print("Você encontrou um par!") 
                 self.pontuacao += 1
-                self.cartas_viradas.clear()
-            else:
+                self.cartas_viradas.clear() # limpa a lista 
+            else: # verifica se o que está a cima não aconteceu e printa essa mensagem
                 print("Não é um par. Memorize as cartas e tente novamente!")
-                time.sleep(3)
-                c1.esconder()
-                c2.esconder()
-                self.cartas_viradas.clear()
+                time.sleep(3) # dá uma pausa do tabuleiro aberto por 3 segundos 
+                c1.esconder() # esconde a carta 1 
+                c2.esconder() # esconde a carta 2 
+                self.cartas_viradas.clear() # limpa a lista 
   
